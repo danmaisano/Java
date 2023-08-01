68ba1b259dfaaa764c75e47a337e8cfc53e51700
Dan Maisano <danmaisano@gmail.com>
2023-08-01 14:48:06 -0400

    pushing

---
+++ cafeJava/CafeJava.java
@@ -0,0 +1,49 @@
+public class CafeJava {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
+        
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+        double dripCoffee = 2.0;
+        double latte = 3.0;
+        double cappuccino = 3.0;
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = false;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = true;
+    
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        System.out.println(customer1 + pendingMessage); 
+        if (isReadyOrder4 == true){
+            System.out.println(customer4 + readyMessage + ". " + displayTotalMessage + cappuccino);
+        }
+        else {
+            System.out.println(customer4 + pendingMessage);
+        }
+        System.out.println(displayTotalMessage + latte*2 +", " + customer2);
+        isReadyOrder2 = true;
+        if (isReadyOrder2 == true){
+            System.out.println(customer2 + readyMessage);
+        }
+        System.out.println(displayTotalMessage + (latte - dripCoffee) + ", " + customer3);
+
+
+
+    	// ** Your customer interaction print statements will go here ** //
+    }
+}
