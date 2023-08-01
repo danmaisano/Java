eeb1b219cccdbd81cc00c08983cec11d2675bf4d
Dan Maisano <danmaisano@gmail.com>
2023-08-01 13:40:22 -0400

    first commit

---
+++ firstJavaProgram/Info.java
@@ -0,0 +1,7 @@
+public class Info {
+    public static void main(String[] args) {
+        System.out.println("My name is Dan");
+        System.out.println("I am 37 years old");
+        System.out.println("My hometown in Washington, DC");
+    }
+}
